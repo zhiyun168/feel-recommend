1267edfa253341dfa0fa779f419849dcb3a82896
canoe <canoefzh@gmail.com>
2015-12-18 16:46:30 +0800

    add persist level

--- src/main/scala/com/feel/statistics/FeelUserPageRank.scala
+++ src/main/scala/com/feel/statistics/FeelUserPageRank.scala
@@ -2,6 +2,7 @@ package com.feel.statistics
 
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx.{Edge, Graph}
+import org.apache.spark.storage.StorageLevel
 
 /**
   * Created by canoe on 12/11/15.
@@ -16,7 +17,8 @@ object FeelUserPageRank {
       .filter(_.length == 2)
       .map(x => new Edge(x(1).toLong, x(0).toLong, 1L))
 
-    val graph = Graph.fromEdges(followEdge, None)
+    val graph = Graph.fromEdges(followEdge, None, edgeStorageLevel = StorageLevel.MEMORY_AND_DISK,
+    vertexStorageLevel = StorageLevel.MEMORY_AND_DISK)
     val rank = graph.pageRank(0.0001, 0.15)
     rank.vertices.saveAsTextFile(args(1))
   }
