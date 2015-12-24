3aca7015068ada8f31b0b7f90e7be07c46b83947
canoe <canoefzh@gmail.com>
2015-12-24 12:24:22 +0800

    cluster info & norm

--- src/main/scala/com/feel/statistics/StepGame.scala
+++ src/main/scala/com/feel/statistics/StepGame.scala
@@ -37,24 +37,48 @@ object StepGame {
      }).groupByKey()
       .map(x => {
         (x._1._1, (x._1._2, x._2.foldLeft(0D)((acc, value) => acc + value) / x._2.size))
-      }).groupByKey()
+      })
+      .groupByKey()
+      .filter(_._2.map(_._2).max > 500D)
       .map(x => {
-        (x._1, Vectors.dense(x._2.toArray.sortWith(_._1 < _._1).map(_._2)))
+        val maxValue = x._2.map(_._2).max
+        (x._1, Vectors.dense(x._2.toArray.sortWith(_._1 < _._1).map(_._2 / maxValue)))
       })
 
     args(5) match {
       case "kmeans" => {
         val model = KMeans.train(userStepNumber.map(_._2), args(3).toInt, args(4).toInt)
-        userStepNumber.map(x => {
-          (model.predict (x._2), (x._2, x._1) )
+        userStepNumber.flatMap(x => {
+          val cluster = model.predict(x._2)
+          x._2.toArray.zipWithIndex.map(y => ((cluster, y._2), y._1))
+        }).groupByKey()
+        .map(x => {
+          val cluster = x._1._1
+          val hour = x._1._2
+          val hourStepMean = x._2.foldLeft(0D)((acc, value) => acc + value) / x._2.size
+          (cluster, (hour, hourStepMean))
+        }).groupByKey()
+        .map(x => {
+          x._1 + "\t" + x._2.toSeq.sortWith(_._1 < _._1).map(_._2).mkString(";")
         }).saveAsTextFile(args(2))
+
       }
       case "gmm" => {
-        val gmm = new GaussianMixture().setK(args(3).toInt).setMaxIterations(args(4).toInt)
+        val model = new GaussianMixture().setK(args(3).toInt).setMaxIterations(args(4).toInt)
           .run(userStepNumber.map(_._2))
-        userStepNumber.map(x => {
-          (gmm.predict(x._2), (x._2, x._1))
-        }).saveAsTextFile(args(2))
+        userStepNumber.flatMap(x => {
+          val cluster = model.predict(x._2)
+          x._2.toArray.zipWithIndex.map(y => ((cluster, y._2), y._1))
+        }).groupByKey()
+          .map(x => {
+            val cluster = x._1._1
+            val hour = x._1._2
+            val hourStepMean = x._2.foldLeft(0D)((acc, value) => acc + value) / x._2.size
+            (cluster, (hour, hourStepMean))
+          }).groupByKey()
+          .map(x => {
+            x._1 + "\t" + x._2.toSeq.sortWith(_._1 < _._1).map(_._2).mkString(";")
+          }).saveAsTextFile(args(2))
       }
     }
   }
