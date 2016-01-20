f3d64d6c4423d4b40aad953672b771c18246862c
canoe <canoefzh@gmail.com>
2016-01-20 15:35:15 +0800

    rt

---
+++ src/main/scala/com/feel/statistics/AllReport.scala
@@ -0,0 +1,20 @@
+package com.feel.statistics
+
+import org.apache.spark.SparkContext
+
+/**
+  * Created by canoe on 1/20/16.
+  */
+object AllReport {
+
+  def main(args: Array[String]) = {
+    val sc = new SparkContext()
+
+    val data = sc.textFile(args(0))
+      data.map(_.split("\t"))
+          .map(x => (x(0), x(1)))
+          .groupByKey()
+          .map(x => "uid:" + x._1 + ";" + x._2.toSeq.mkString(","))
+          .saveAsTextFile(args(1))
+  }
+}
