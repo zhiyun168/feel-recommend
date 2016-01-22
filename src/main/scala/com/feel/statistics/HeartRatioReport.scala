10eb4e10fe7afa16adcaba963761e05bf4ea2014
canoe <canoefzh@gmail.com>
2016-01-22 12:27:45 +0800

    heart ratio

--- src/main/scala/com/feel/statistics/HeartRatioReport.scala
+++ src/main/scala/com/feel/statistics/HeartRatioReport.scala
@@ -1,5 +1,8 @@
 package com.feel.statistics
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import com.feel.utils.TimeIssues
 import org.apache.hadoop.conf.Configuration
 import org.apache.spark.SparkContext
@@ -22,25 +25,70 @@ object HeartRatioReport {
     val startTime = TimeIssues.nDaysAgoTs(7)
     val endTime = TimeIssues.nDaysAgoTs(0)
 
-    val userDistance = mongoRDD.filter(x => {
+    val userHeartRatio = mongoRDD.filter(x => {
       val ts = x._2.get("record_time").toString.toLong / 1000
       ts >= startTime && ts < endTime
     }).map(x => {
       try {
         val user = x._2.get("uid").toString
-        val heartRatio = x._2.get("info").asInstanceOf[BSONObject].get("bpm").toString
+        val heartRatio = x._2.get("info").asInstanceOf[BSONObject].get("bpm").toString.toDouble
         (user, heartRatio)
       } catch {
-        case _: Throwable => ("", "")
+        case _: Throwable => ("", 0D)
       }
-    }).filter(_._2 != "")
+    }).filter(_._2 > 0D)
       .groupByKey()
       .map(x => {
         val user = x._1
-        val variance = x._2.toSeq.mkString(",")
-        (user, variance)
+        val minHeartRatio = x._2.min
+        val maxHeartRatio = x._2.max
+        val meanHeartRatio = x._2.foldLeft(0D)((acc, value) => acc + value) / x._2.size
+        (user, (minHeartRatio, maxHeartRatio, meanHeartRatio))
       })
-    userDistance.saveAsTextFile(args(2))
 
+    userHeartRatio.map(x => {
+      val user = x._1
+      user + "\tuser_heart_ratio:" + x._2._1.toString + "," + x._2._2.toString + "," + x._2._3.toString
+    }).saveAsTextFile(args(2))
+
+
+    val userInfo = sc.textFile(args(3))
+      .map(_.split("\t"))
+      .filter(_.length == 3)
+      .map(x => {
+        val user = x(0)
+        val gender = x(1)
+        val birthday = x(2)
+        val pattern = "([0-9]+)-([0-9][0-9])-([0-9][0-9])".r
+        val age = birthday match {
+          case pattern(birthdayYear, birthdayMonth, birthdayDay) => {
+            val today = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+            val yearDiff = today.substring(0, 4).toInt - birthdayYear.toInt
+            val delta = {
+              if (today.substring(5, 10) > birthdayMonth + "-" + birthdayDay) 1
+              else 0
+            }
+            if (yearDiff > 100 || yearDiff < 3) 0 else yearDiff + delta
+          }
+          case _ => 0
+        }
+        if (gender == "x" || age == 0) {
+          (user, "default")
+        } else {
+          (user, gender + ";" + age.toString)
+        }
+      })
+    val infoUserHeartRatioDiff = userHeartRatio.map(x => (x._1, x._2._3))
+      .join(userInfo)
+      .map({ case (user, (heartRatio, info)) =>
+        (info, (user, heartRatio))
+      }).groupByKey()
+      .flatMap(x => {
+        val infoUserHeartRatioMean = x._2.foldLeft(0D)((acc, value) => acc + value._2) / x._2.size
+        x._2.map({ case (user, heartRatio) =>
+          user + "\tuser_heart_ratio_gap:" + x._1 + "," + (heartRatio - infoUserHeartRatioMean).formatted("%.2f")
+        })
+      })
+    infoUserHeartRatioDiff.saveAsTextFile(args(4))
   }
 }
