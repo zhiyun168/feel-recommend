7b27daee9b26ddc95b53bd412914f77c09d034e5
canoe <canoefzh@gmail.com>
2016-02-17 10:06:20 +0800

    add time parameter

--- src/main/scala/com/feel/statistics/DistanceReport.scala
+++ src/main/scala/com/feel/statistics/DistanceReport.scala
@@ -19,8 +19,8 @@ object DistanceReport {
     val mongoRDD = sc.newAPIHadoopRDD(hadoopConf, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object],
       classOf[BSONObject])
 
-    val startTime = TimeIssues.nDaysAgoTs(7)
-    val endTime = TimeIssues.nDaysAgoTs(0)
+    val startTime = TimeIssues.nDaysAgoTs(args(4).toInt)
+    val endTime = TimeIssues.nDaysAgoTs(args(5).toInt)
 
     val userDistance = mongoRDD.map(x => {
       val user = x._2.get("uid").toString
